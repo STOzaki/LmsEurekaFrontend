6b7e8481c0658fb920b8260e44f4f71481349c06
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-05-01 13:30:52 -0400

    login validation

---
+++ js/adminLogin.js
@@ -0,0 +1,43 @@
+var users = [{
+  username: 'test',
+  password: 's'
+}];
+window.localStorage.clear();
+window.localStorage.setItem('users', JSON.stringify(users));
+
+function validateLogin() {
+  var usernameHtml, passwordHtml, username, password, errorHtml;
+  usernameHtml = document.getElementById("username");
+  passwordHtml = document.getElementById("password");
+  username = usernameHtml.value;
+  password = passwordHtml.value;
+
+  errorHtml = document.getElementById("error");
+
+  const users = JSON.parse(window.localStorage.getItem('users'));
+
+  // check credentials
+  if(users != null) {
+    const arrayLength = users.length;
+    let i;
+    let successful = false;
+    for(user of users) {
+      console.log(user);
+      if(user.username == username && user.password == password) {
+        window.localStorage.setItem('currentUser', JSON.stringify({
+          username: username
+        }));
+        return true;
+      } else {
+        errorHtml.parentElement.style.display = "flex";
+        errorHtml.innerHTML = "Either your password or username is incorrect";
+      }
+    }
+  } else {
+    console.log("we do not have any users!!");
+    errorHtml.parentElement.style.display = "flex";
+    errorHtml.innerHTML = "we had a problem with our \"server\"";
+  }
+
+  return false;
+}
